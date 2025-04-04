d94622d832bde755c78aa4b108019ee97fcb36f9
Bestin John <bestin.john@iese.fraunhofer.de>
2025-04-04 15:56:10 +0200

    feat: refactor appRouter to use todoRouter and remove inline CRUD operations

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,40 +1,8 @@
-import { z } from "zod";
-import { publicProcedure, router } from "./trpc";
+import { todoRouter } from "./todo";
+import { router } from "./trpc";
 
 export const appRouter = router({
-  getTodos: publicProcedure.query(() => {
-    return [
-      {
-        id: 1,
-        title: "Buy milk",
-        completed: false,
-      },
-      {
-        id: 2,
-        title: "Buy eggs",
-        completed: false,
-      },
-    ];
-  }),
-  createTodo: publicProcedure
-    .input(z.object({ title: z.string() }))
-    .mutation(() => {
-      return "Created";
-    }),
-  updateTodo: publicProcedure
-    .input(
-      z.object({
-        id: z.number(),
-        title: z.string().optional(),
-        completed: z.boolean().optional(),
-      })
-    )
-    .mutation(() => {
-      return "Updated";
-    }),
-  deleteTodo: publicProcedure.input(z.number()).mutation(() => {
-    return "Deleted";
-  }),
+  todo: todoRouter,
 });
 
 export type AppRouter = typeof appRouter;
