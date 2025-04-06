d2ab487f229855ac66fdfddc5571a6d1b02b1f02
Bestin John <bestinjohn1993@gmail.com>
2025-04-06 19:22:27 +0200

    refactor: update RootLayout and Home components to use arrow function syntax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import TodoList from "./_components/TodoList";
 import { serverClient } from "./_trpc/serverClient";
 
-export default async function Home() {
+const Home = async () => {
   const todos = await serverClient.todo.getTodos();
 
   return (
     <>
       <header>Header</header>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col">
         <TodoList todos={todos} />
       </main>
       <footer>Footer</footer>
     </>
   );
-}
+};
+
+export default Home;
